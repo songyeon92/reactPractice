3f07b0694eb5a2010e2634a597a3d1d0c075dbba
전송연 <SongYeon@jeonsong-yeon-ui-MacBook-Pro.local>
2021-06-27 23:21:28 +0900

    Added Mapping, props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+const newNotes = (note) => {
+    return <Note 
+        title={note.title}
+        content={note.content}
+    />
+}
 
 const App = () => {
     return (
         <div>
             <Header />
-            <Note />
+            { notes.map(newNotes) }
             <Footer />
         </div>
     );
